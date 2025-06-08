a43fe6f7dff7b4920e3cc2178296d73435aaaaba
endless296 <name86473@gmail.com>
2025-06-08 16:57:38 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
+import router from './router/index.js';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(router)   // <-- tell Vue to use the router
+  .mount('#app');
